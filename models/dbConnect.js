1fe6f639f6ccddcb71683eec6819b4e23298cd37
AhmedMagdy25 <117306580+AhmedMagdy25@users.noreply.github.com>
2022-12-03 13:18:44 +0200

    commit

--- models/dbConnect.js
+++ models/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = cb=>{
         return new Promise(async (resolve, reject)=>{
-                await mongoose.connect('mongodb+srv://AhmedMagdy:WcGeei0gskICYv3a@cluster0.dwpglrg.mongodb.net/?retryWrites=true&w=majority/mvc-project')
+                await mongoose.connect('mongodb+srv://AhmedMagdy:WcGeei0gskICYv3a@cluster0.dwpglrg.mongodb.net/mvc-project?retryWrites=true&w=majority')
                 .then(()=>{
                         return cb()
                         .then((resalt)=>{
